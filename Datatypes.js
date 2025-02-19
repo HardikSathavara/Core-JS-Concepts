4a48825254b74d92f328ccc7f57c2a772ca79961
Hardik Sathavara <hardiksathavara00h@gmail.com>
2025-02-20 00:31:23 +0530

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,77 @@
+// String
+// Number, Integer
+// Float
+// Boolean, Bool
+// Array, List
+// Object, Dictionary
+// null, None
+// undefined
+
+
+// String 
+
+var a = "h,a,r,d,i,k"
+var b = 'Megha'
+
+a[1] = ","
+
+
+// Number 
+
+var a = 10
+
+// Float 
+
+var a = 10.00
+
+
+// Boolean 
+
+var a = true
+var b = false
+
+// Array 
+
+var a = [1,2,"hardik",[222,555,777], {"name":"megha"}]
+
+a.map((i)=> {console.log(i)})
+
+// Object 
+
+var a = {
+    name: "hardik",
+    age: 25,
+    skills: ["python", "JS"],
+}
+
+// a.name = "hardik"
+// a['name'] = "hardik"
+
+// // var age = 26
+
+// // var a = {age:26}
+
+// a['age'] = 36
+
+// a['email'] = "fdfdfd"
+
+// a.keys() --> ["name", "age", "skills"] 
+// a.values() --> ["hardik", 36, ["python", "JS"]]
+
+
+
+Object.entries(a).map(([key, value]) => {console.log(`KEY: ${key}, VALUE: ${value}`)})
+
+
+
+const url = "https://www.google.com"
+
+function abc(){
+
+    var a = 10
+    let b = 20
+    const c = 30
+
+
+}
+
